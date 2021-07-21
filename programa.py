dc4c7328e759b4bfffa923f6bc7baa63bac983bc
fransobral <chulosobral@gmail.com>
2021-07-21 17:22:29 -0300

    Mejore la listada de archivos con mas opciones y todo mas orgaizado.

--- programa.py
+++ programa.py
@@ -8,6 +8,7 @@ def verificacion_eleccion(numero:int) -> tuple:
     Pre: Le ingreso el numero de opcion elegida por el usuario.
     Post: Imprimo un confirmacion de la eleccion del usuario
     """
+    correcta_eleccion = False
     opcion_correcta = input(f"\nUsted a elegido la opcion {numero}. Esta seguro que desa continuar? (si/no): ")
     if opcion_correcta == "si" or opcion_correcta == "Si":
         correcta_eleccion = True
@@ -148,27 +149,119 @@ def eleccion_crear_archivo_o_carpeta(drive_service) -> None:
             if repeticion != "si":
                 decision = repeticion
     
-def listar_archivos_drive(drive_service)-> None: #ver como elegir la carpeta a mostrar
+def listar_archivos_drive(drive_service)-> None: 
     """ 
     Pre: Recibe lo servicios de google drive.
     Post: Printea los archivos de la capeta especificada por el usuario.
     """
-    
-    results = drive_service.files().list(fields="nextPageToken, files(id, name)").execute()
-    carpetas = drive_service.files().list(q="mimeType='application/vnd.google-apps.folder'",
-                                            spaces='drive',
-                                            fields='nextPageToken, files(id, name)',).execute()
-    for file in carpetas.get('files', []):
-        # Process change
-        print('Carpeta encontrada: %s (%s)\n' % (file.get('name'), file.get('id')))
+    listar = True
+    archivos = drive_service.files().list(fields="nextPageToken, files(id, name, mimeType)").execute()
 
-    items = results.get('files', [])
-    if not items:
-        print('\nNo se encontraron archivos.\n')
+    while listar:
+        ids_carpetas,ids_archivos = separador_archivos_carpetas(archivos)
+        imprimir_carpetas(ids_carpetas)
+        imprimir_archivos(ids_archivos)
+
+        decision,carpetas = verificador_de_carpetas(ids_carpetas)
+        
+        if decision == "a":
+            carpeta = input("Introduci el numero de la carpeta: ")
+            carpeta = conversor_int(carpeta)
+            id_carpeta = ids_carpetas[carpeta-1][0]
+            archivos = drive_service.files().list(q= f"'{id_carpeta}' in parents",fields="nextPageToken, files(id, name, mimeType)").execute()
+        elif decision == "b":
+            obtener_ids(carpetas,ids_archivos,ids_carpetas)
+            listar = False
+        else:
+            print("\nUsted eligio vover al menu principal.\n")
+            listar = False
+
+def conversor_int(numero:str) -> int:
+    """ 
+    Pre: Recibo un str.
+    Post: Si es un numero lo transformo a int, en caso contario le pido devuelta un numero.
+    """
+    es_numero = False
+
+    while es_numero == False:
+        if numero.isnumeric():
+            numero = int(numero) 
+            es_numero = True
+        else:
+            numero = input("El valor ingresado no es un numero, por favor intente nuevamente: ")
+    return numero
+
+def obtener_ids(carpeta:bool,ids_archivos:list,ids_carpetas:list) -> None:
+    """ 
+    Pre: Recibe las listas de archivos y el booleano carpeta.
+    Post: Le pregunta al usuario que id quiere obtener (en caso de que no haya carpetas asume que es de archivos) y lo imprime.
+    """
+    if carpeta: #si hay carpetas te pregunta si queres obtener el id, sino lo evita y asume que queres el id de un archivo
+        decision_2 = input("\nDesea obtener el id de un archivo o carpeta? (a/b): ")
     else:
-        print('\nArchivos:\n')
-        for item in items:
-            print(f"{item['name']} ({item['id']})\n")
+        decision_2 = "a"
+
+    if decision_2 == "a":
+        num_archivo = input("\nIngrese el numero del archivo que desea obtener el id: ")
+        num_archivo = conversor_int(num_archivo)
+        print(f"\nEl id del archivo {num_archivo} es: {ids_archivos[num_archivo-1][0]}")
+    else:
+        num_carpeta = input("\nIngrese el numero de carpeta que desea obtener el id: ")
+        num_carpeta = conversor_int(num_carpeta)
+        print(f"\nEl id de la carpeta {num_carpeta} es: {ids_carpetas[num_carpeta-1][0]}")
+
+def separador_archivos_carpetas(archivos)-> list:
+    """ 
+    Pre: Recibo un conjunto de archivos.
+    Post: Separo esos dos conjuntos en archivos y carpetas y los almaceno en las listas creadas.
+    """
+    ids_carpetas = list()
+    ids_archivos = list()
+    numero = 0
+    for file in archivos.get('files', []): 
+        numero += 1
+        if file.get("mimeType") == "application/vnd.google-apps.folder":
+            ids_carpetas.append((file.get('id'),file.get('name')))
+        else: 
+            ids_archivos.append((file.get('id'),file.get('name')))
+    return ids_carpetas,ids_archivos
+
+def verificador_de_carpetas(ids_carpetas:list)-> tuple:
+    """ 
+    Pre: Recibe una lista de carpetas.
+    Post: Verifica que esa lista tenga contenido y devuelve un str que sera usado en el programa principal.
+    """
+    carpetas = True
+    if not ids_carpetas:
+        decision = input("Desea obtener algun id o volver al menu principal? (a/b): ")
+        if decision == "a":
+            decision = "b"
+            carpetas = False
+        elif decision == "b":
+            decision = "c"
+    else:
+        decision = input("Desea entrar en una carpeta, obtener algun id o volver al menu principal? (a/b/c): ")
+    return decision,carpetas
+
+def imprimir_carpetas(ids_carpetas:list)-> None:
+    """ 
+    Pre: Recibe una lista de carpetas.
+    Post: Imprime esa lista separada por un numero inicial.
+    """
+    numero = 0
+    for file in ids_carpetas: #Imprime las carpetas
+        numero += 1
+        print(f'\nCarpeta Nº {numero}: {ids_carpetas[numero-1][1]}')
+
+def imprimir_archivos(ids_archivos:list)-> None:
+    """ 
+    Pre: Recibe una lista de archivos.
+    Post: Imprime esa lista separada por un numero inicial.
+    """
+    numero = 0
+    for file in ids_archivos: 
+        numero += 1
+        print(f'\nArchivo Nº {numero}: {ids_archivos[numero-1][1]}')
 
 def crear_carpeta_drive(drive_service,nombre_carpeta:str)-> None: 
     """ 
